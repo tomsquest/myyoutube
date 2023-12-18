c1bd7a2fdbf85bd70d3e9cd5c44dc66985e115b2
Thomas Queste <tom@tomsquest.com>
2023-12-18 18:37:38 +0100

    wip: get list of videos from channel

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello via Bun!");
+import axios from "axios";
+
+const API_KEY = process.env.YOUTUBE_API_KEY;
+const CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID;
+
+type Video = {
+  id: {
+    videoId: string;
+  },
+  snippet: {
+    title: string;
+  }
+}
+
+const getChannelVideos = async () => {
+  console.log("Will list videos of channel");
+  const maxResults = 20;
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=id,snippet&order=date&maxResults=${maxResults}`;
+
+  try {
+    const response = await axios.get<{items: Video[]}>(url);
+    const videos = response.data.items;
+
+    videos.forEach((video, index:number) => {
+      console.log(`[${String(index).padStart(4)}] ${video.snippet.title}, ID: ${video.id.videoId}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getChannelVideos();
