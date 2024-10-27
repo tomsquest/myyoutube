92136981bf8730f5394ccabbbeb01fd70725201d
Thomas Queste <tom@tomsquest.com>
2024-10-27 14:33:30 +0100

    feat: list all videos of a channel

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { pipe } from "fp-ts/function";
 import * as TE from "fp-ts/TaskEither";
 
-const API_KEY = process.env.YOUTUBE_API_KEY;
-const CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID;
+const API_KEY = process.env.YOUTUBE_API_KEY!;
+const CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID!;
 
 type SearchListResponse = {
   items: SearchResultResponse[];
@@ -37,58 +37,113 @@ type VideoGetRatingResponse = {
   }[];
 };
 
-const getChannelVideos = (
+type PlaylistItemListResponse = {
+  kind: "youtube#playlistItemListResponse";
+  etag: string;
+  nextPageToken: string;
+  prevPageToken: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: PlaylistItemsResponse[];
+};
+
+type PlaylistItemsResponse = {
+  kind: "youtube#playlistItem";
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    channelTitle: string;
+    videoOwnerChannelTitle: string;
+    videoOwnerChannelId: string;
+    playlistId: string;
+    position: number;
+    resourceId: {
+      kind: string;
+      videoId: string;
+    };
+  };
+  contentDetails: {
+    videoId: string;
+    startAt: string;
+    endAt: string;
+    note: string;
+    videoPublishedAt: string;
+  };
+  status: {
+    privacyStatus: string;
+  };
+};
+
+type ChannelVideo = {
+  id: string;
+  title: string;
+  description: string;
+  publishedAt: string;
+};
+
+const listAllChannelVideos = (): TE.TaskEither<Error, ChannelVideo[]> => {
+  const apiKey = API_KEY;
+  const playlistId = "UU" + CHANNEL_ID.slice(2);
+  const allVideos: ChannelVideo[] = [];
+
+  const listByPage = (
+    pageToken?: string,
+  ): TE.TaskEither<Error, ChannelVideo[]> =>
+    pipe(
+      listChannelVideos(apiKey, playlistId, pageToken),
+      TE.chain((playlistPage) => {
+        const videos = playlistPage.items.map((video) => {
+          return {
+            id: video.snippet.resourceId.videoId,
+            title: video.snippet.title,
+            description: video.snippet.description,
+            publishedAt: video.snippet.publishedAt,
+          };
+        });
+
+        allVideos.push(...videos);
+
+        return playlistPage.nextPageToken
+          ? listByPage(playlistPage.nextPageToken)
+          : TE.right(allVideos);
+      }),
+    );
+
+  return listByPage();
+};
+
+const listChannelVideos = (
+  apiKey: string,
+  playlistId: string,
   pageToken?: string,
-): TE.TaskEither<Error, SearchResultResponse[]> => {
+): TE.TaskEither<Error, PlaylistItemListResponse> => {
   const maxResults = 50; // max is 50
 
-  let url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=id,snippet&order=date&type=video&maxResults=${maxResults}`;
+  // Doc: https://developers.google.com/youtube/v3/docs/search/list
+  // Doc: https://developers.google.com/youtube/v3/docs/playlistItems/list
+  let url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=${maxResults}&playlistId=${playlistId}&key=${apiKey}`;
 
+  // no pageToken means the first page
   if (pageToken) {
     url += `&pageToken=${pageToken}`;
   }
 
-  return pipe(
-    TE.tryCatch(
-      () => axios.get<SearchListResponse>(url),
-      (reason) => new Error(String(reason)),
-    ),
-    TE.chain(({ data }) => {
-      const videoIds = data.items.map((video) => video.id.videoId).join(",");
-      return pipe(
-        TE.tryCatch(
-          () =>
-            axios.get<VideoGetRatingResponse>(
-              `https://www.googleapis.com/youtube/v3/videos/getRating?key=${API_KEY}&id=${videoIds}`,
-            ),
-          (reason) => new Error(String(reason)),
-        ),
-        TE.map((ratingResponse) => {
-          data.items.forEach((video) => {
-            const ratingItem = ratingResponse.data.items.find(
-              (item) => item.videoId === video.id.videoId,
-            );
-            if (ratingItem) {
-              video.rating = ratingItem.rating;
-            }
-          });
-          return data.items;
-        }),
-        TE.chain((videos) =>
-          data.nextPageToken
-            ? pipe(
-                getChannelVideos(data.nextPageToken),
-                TE.map((nextPageVideos) => [...videos, ...nextPageVideos]),
-              )
-            : TE.right(videos),
-        ),
-      );
-    }),
+  return TE.tryCatch(
+    async () => {
+      const { data } = await axios.get<PlaylistItemListResponse>(url);
+      return data;
+    },
+    (reason) => new Error(String(reason)),
   );
 };
 
 const main = pipe(
-  getChannelVideos(),
+  listAllChannelVideos(),
   TE.fold(
     (error) => {
       console.error(`Failed to fetch videos: ${error.message}`);
@@ -98,11 +153,7 @@ const main = pipe(
       TE.fromIO(() => {
         videos.forEach((video, index) => {
           console.log(
-            `[${String(index + 1).padStart(4)}] ${video.id.kind} ${
-              video.snippet.publishedAt
-            } ${video.snippet.title}, ID: ${video.id.videoId} Rating: ${
-              video.rating
-            }`,
+            `[${String(index + 1).padStart(4)}] ${video.id} ${video.publishedAt} ${video.title}`,
           );
         });
       }),
